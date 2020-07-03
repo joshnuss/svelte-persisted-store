d904b06c5ca2d69569db7975350b7b0b2a5ac225
Josh Nussbaum <joshnuss@gmail.com>
2020-07-03 01:02:44 -0400

    Adds writable localStorageStore

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import {writable as internal, get} from 'svelte/store'
+
+export function writable(key, initialValue) {
+  const store = internal(initialValue)
+  const {subscribe, set} = store
+  const json = localStorage.getItem(key)
+
+  if (json) {
+    set(JSON.parse(json))
+  }
+
+  return {
+    set(value) {
+      localStorage.setItem(key, JSON.stringify(value))
+      set(value)
+    },
+    update(cb) {
+      const value = cb(get(store))
+      this.set(value)
+    },
+    subscribe
+  }
+}

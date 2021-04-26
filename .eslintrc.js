f1f4ea1630997448a758db5dbaca97081940c751
Ruben Hensen <ruben.hensen@protonmail.com>
2021-04-26 21:53:26 +0200

    Fix tests and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,26 +11,8 @@ module.exports = {
     '@typescript-eslint'
   ],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  rules: {
-    indent: [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    quotes: [
-      'error',
-      'single'
-    ],
-    semi: [
-      'error',
-      'never'
-    ]
-  }
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ]
 }

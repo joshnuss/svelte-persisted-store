0881ed2e4645c7f984e8db7a35f935a8d6748478
Ruben Hensen <ruben.hensen@protonmail.com>
2021-04-30 01:07:09 +0200

    Turned ternary into if statement and added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,34 @@ module.exports = {
     browser: true,
     amd: true,
     node: true
-},
+  },
   root: true,
-  ignorePatterns: ["dist/"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['dist/'],
+  parser: '@typescript-eslint/parser',
   plugins: [
-    "@typescript-eslint"
+    '@typescript-eslint'
   ],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
-  ]
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'never'
+    ]
+  }
 }
